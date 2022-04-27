7a3f01956ff4637aa550ae8edb863a3ce9b012ff
Matthew Ault <matthew.ault@segment.com>
2022-04-27 17:18:11 -0400

    unmarshal webhook data into object

--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.16
 
 require (
 	github.com/cocoonlife/timber v0.0.0-20180608095500-d53b6a75f0c2
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/now v1.1.5 // indirect
+	gorm.io/gorm v1.23.5 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -1,11 +1,14 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 
 	log "github.com/cocoonlife/timber"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/gorilla/mux"
 )
 
@@ -36,7 +39,8 @@ func (a *App) Init(portNum string) error {
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	router.HandleFunc("/hello", a.Hello)
+	router.HandleFunc("/", a.Hello)
+	router.HandleFunc("/webhooks", a.Webhooks)
 
 	a.server = server
 	return nil
@@ -49,4 +53,96 @@ func (a *App) Run() error {
 
 func (a *App) Hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "Hello world!")
+}
+
+// Event represents a slack event data type which is received by the webhooks
+// endpoint
+type Event struct {
+	APIAppID       string `json:"api_app_id"`
+	Authorizations []struct {
+		EnterpriseID        interface{} `json:"enterprise_id"`
+		IsBot               bool        `json:"is_bot"`
+		IsEnterpriseInstall bool        `json:"is_enterprise_install"`
+		TeamID              string      `json:"team_id"`
+		UserID              string      `json:"user_id"`
+	} `json:"authorizations"`
+	Event struct {
+		CacheTs int    `json:"cache_ts"`
+		EventTs string `json:"event_ts"`
+		Type    string `json:"type"`
+		User    struct {
+			Color             string `json:"color"`
+			Deleted           bool   `json:"deleted"`
+			ID                string `json:"id"`
+			IsAdmin           bool   `json:"is_admin"`
+			IsAppUser         bool   `json:"is_app_user"`
+			IsBot             bool   `json:"is_bot"`
+			IsEmailConfirmed  bool   `json:"is_email_confirmed"`
+			IsOwner           bool   `json:"is_owner"`
+			IsPrimaryOwner    bool   `json:"is_primary_owner"`
+			IsRestricted      bool   `json:"is_restricted"`
+			IsUltraRestricted bool   `json:"is_ultra_restricted"`
+			Locale            string `json:"locale"`
+			Name              string `json:"name"`
+			Profile           struct {
+				AvatarHash             string        `json:"avatar_hash"`
+				DisplayName            string        `json:"display_name"`
+				DisplayNameNormalized  string        `json:"display_name_normalized"`
+				Email                  string        `json:"email"`
+				Fields                 interface{}   `json:"fields"`
+				FirstName              string        `json:"first_name"`
+				Image192               string        `json:"image_192"`
+				Image24                string        `json:"image_24"`
+				Image32                string        `json:"image_32"`
+				Image48                string        `json:"image_48"`
+				Image512               string        `json:"image_512"`
+				Image72                string        `json:"image_72"`
+				LastName               string        `json:"last_name"`
+				Phone                  string        `json:"phone"`
+				RealName               string        `json:"real_name"`
+				RealNameNormalized     string        `json:"real_name_normalized"`
+				Skype                  string        `json:"skype"`
+				StatusEmoji            string        `json:"status_emoji"`
+				StatusEmojiDisplayInfo []interface{} `json:"status_emoji_display_info"`
+				StatusExpiration       int           `json:"status_expiration"`
+				StatusText             string        `json:"status_text"`
+				StatusTextCanonical    string        `json:"status_text_canonical"`
+				Team                   string        `json:"team"`
+				Title                  string        `json:"title"`
+			} `json:"profile"`
+			RealName               string `json:"real_name"`
+			TeamID                 string `json:"team_id"`
+			Tz                     string `json:"tz"`
+			TzLabel                string `json:"tz_label"`
+			TzOffset               int    `json:"tz_offset"`
+			Updated                int    `json:"updated"`
+			WhoCanShareContactCard string `json:"who_can_share_contact_card"`
+		} `json:"user"`
+	} `json:"event"`
+	EventID            string `json:"event_id"`
+	EventTime          int    `json:"event_time"`
+	IsExtSharedChannel bool   `json:"is_ext_shared_channel"`
+	TeamID             string `json:"team_id"`
+	Token              string `json:"token"`
+	Type               string `json:"type"`
+}
+
+func (a *App) Webhooks(w http.ResponseWriter, req *http.Request) {
+	defer req.Body.Close()
+
+	b, err := io.ReadAll(req.Body)
+	// b, err := ioutil.ReadAll(resp.Body)  Go.1.15 and earlier
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var eventObj Event
+	err = json.Unmarshal(b, &eventObj)
+	if err != nil {
+		log.Errorf("error unmarshalling json: %v", err)
+		return
+	}
+
+	// We recommend persisting the following user fields:
+	// id, name, deleted, real_name, tz, profile object (status_text, status_emoji, image_512).
+	spew.Dump(eventObj)
 }
