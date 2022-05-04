4bd0c8208c285c9f15ab0da57c204e2436588d04
Matthew Ault <matthew.ault@segment.com>
2022-05-04 12:27:48 -0400

    optimise inserts using transaction

--- db/db.go
+++ db/db.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"errors"
+
 	"github.com/jmoiron/sqlx"
 )
 
@@ -29,17 +31,23 @@ func (p *Postgres) CreateUsers(users []User) error {
 	// Want to do an upsert as if the service has been down the db may be stale
 	// ON CONFLICT does not seem to work with sqlx namedexec
 	//_, err := p.dbConn.NamedExec(`INSERT INTO users (id, name, deleted, real_name, tz, profile_status_text, profile_status_emoji, profile_image_512) VALUES (:id, :name, :deleted, :real_name, :tz, :profile_status_text, :profile_status_emoji, :profile_image_512) ON CONFLICT (id) DO UPDATE SET name=:name, deleted=:deleted, real_name=:real_name, tz=:tz, profile_status_text=:profile_status_text, profile_status_emoji=:profile_status_emoji, profile_image_512=:profile_image_512`, users)
-	var err error
+	tx, err := p.dbConn.Beginx() // put multiple inserts in transaction to speed up
+	if err != nil {
+		return err
+	}
 	for _, user := range users {
-		// this is inefficient but is only run at app start
-		// TODO: optimise
-		_, err = p.dbConn.Exec(`INSERT INTO users VALUES ($1,$2,$3,$4,$5,$6,$7,$8) ON CONFLICT (id) DO UPDATE SET name=$9, deleted=$10, real_name=$11, tz=$12, profile_status_text=$13, profile_status_emoji=$14, profile_image_512=$15`,
+		_, err = tx.Exec(`INSERT INTO users VALUES ($1,$2,$3,$4,$5,$6,$7,$8) ON CONFLICT (id) DO UPDATE SET name=$9, deleted=$10, real_name=$11, tz=$12, profile_status_text=$13, profile_status_emoji=$14, profile_image_512=$15`,
 			user.ID, user.Name, user.Deleted, user.RealName, user.TZ, user.ProfileStatusText, user.ProfileStatusEmoji, user.ProfileImage512, user.Name, user.Deleted, user.RealName, user.TZ, user.ProfileStatusText, user.ProfileStatusEmoji, user.ProfileImage512)
 		if err != nil {
+			rollbackErr := tx.Rollback()
+			if rollbackErr != nil {
+				err = errors.New(err.Error() + ":" + rollbackErr.Error())
+			}
 			return err
 		}
 	}
-	return nil
+	err = tx.Commit()
+	return err
 }
 
 func (p *Postgres) UpdateUser(user User) error {
@@ -49,6 +57,8 @@ func (p *Postgres) UpdateUser(user User) error {
 // may need pagination here
 func (p *Postgres) GetAllUsers() ([]User, error) {
 	var users []User
+	// if database gets significantly large then we may not want to load all
+	// users into memory at once
 	err := p.dbConn.Select(&users, "SELECT * FROM users ORDER BY id")
 	return users, err
 }
